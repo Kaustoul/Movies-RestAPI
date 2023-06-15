949520ce1d5bedbf8b631b22b29738a3f677f4ae
Radek Svěrák <rasvek@gmail.com>
2023-06-15 15:56:23 +0200

    Updated Database class to work with Movie class

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from movie import Movie
 
 class Database:
   def __init__(self, name):
@@ -7,21 +8,31 @@ class Database:
     self.cur = None
 
   def connect(self):
-    self.con = sqlite3.connect(self.name)
+    self.con = sqlite3.connect(self.name, check_same_thread=False)
     self.cur = self.con.cursor()
 
   def create_table(self):
     self.cur.execute('''CREATE TABLE IF NOT EXISTS movies(
       id INTEGER PRIMARY KEY NOT NULL,
+      title VARCHAR(255),
       description TEXT,
       release_year INTEGER NOT NULL CHECK(length(release_year) = 4)
       )''')
     self.con.commit()
     
-  def insert_movie(self, movie_json):
-    self.cursor.execute("INSERT INTO movies (name, description, release_year) VALUES (?, ?, ?)",
-                        (movie_json['name'], movie_json['description'], movie_json['release_year']))
-    self.connection.commit()
+  def insert_movie(self, movie):
+    try:
+      self.cur.execute("INSERT INTO movies (title, description, release_year) VALUES (?, ?, ?)",
+                          (movie.title, movie.description, movie.release_year))
+      self.con.commit()
+      return self.cur.lastrowid
+    except Exception as e:
+      print(e)
+      # Check constraint on release_year failed
+      if isinstance(e, sqlite3.IntegrityError) and "CHECK constraint" in str(e):
+        return -2
+      
+      return -1
 
   def get_all_movies(self):
     self.cur.execute("SELECT * FROM movies")
@@ -33,10 +44,10 @@ class Database:
   
   def update_movie(self, id, movie_json):
     self.cursor.execute('''INSERT INTO movies 
-      (name, description, release_year) 
+      (title, description, release_year) 
       VALUES (?, ?, ?) 
       WHERE id=?''',
-      (movie_json['name'], 
+      (movie_json['title'], 
        movie_json['description'], 
        movie_json['release_year'], 
        id))
